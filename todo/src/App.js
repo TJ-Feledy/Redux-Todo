d53b356e0fd85e65d5519c8f115011bd3bab768a
teej387 <teej387@yahoo.com>
2019-07-12 19:15:08 -0500

    added moment.js into App

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Today's list of important things To Do!</h1>
+      <div className='todaysDate'>{moment().format('MMM Do YYYY')}</div>
+
     </div>
   );
 }
