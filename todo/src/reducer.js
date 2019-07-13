9b3af4c4fb7980bf75d8d8d7f815d2bb6ca39479
teej387 <teej387@yahoo.com>
2019-07-12 19:44:23 -0500

    imported action ADD_TASK into reducer

--- todo/src/reducer.js
+++ todo/src/reducer.js
@@ -1,11 +1,21 @@
-
+import {ADD_TASK} from './actions'
 
 const initialState = {
   todos: []
 }
 
 export default function(state= initialState, action) {
-  
-  
-    return state
+  switch (action.type) {
+    case ADD_TASK: {
+      const { task } = action.payload
+      const newTask = state.todos.concat([task])
+
+      return {
+        ...state,
+        todos: newTask,
+      }
+    }
+    default:
+      return state
+  }
 }
