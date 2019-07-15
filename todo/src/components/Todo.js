610ed6c7105a05ededda85a64076d09d1c69866c
teej387 <teej387@yahoo.com>
2019-07-15 01:16:41 -0500

    Created deleteButton

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {deleteTask} from '../actions'
 
 function Todo(props) {
 
@@ -6,11 +8,33 @@ function Todo(props) {
     props.toggleComplete(props.item.id)
   }
 
+  const submitHandler = (evt) => {
+    evt.preventDefault()
+    removeTask(evt.target.id)
+    console.log(evt.target.id)
+  }
+
+  const removeTask = (id) => {
+    const newTaskList = props.todos.filter(task => task.id !== id)
+    props.deleteTask(newTaskList)
+  }
+
   return (
     <li className='todo'>
       <p className={`taskItem ${props.item.completed ? 'completed' : ''}`} onClick={clickHandler} >{props.item.value}</p>
+      <button className='deleteButton' type='button' onClick={submitHandler} >Delete</button>
     </li>
   );
 };
 
-export default Todo;
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos,
+  }
+}
+
+const mapDispatchToProps = {
+  deleteTask: deleteTask,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todo);
